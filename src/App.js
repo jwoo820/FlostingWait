2416d74978a26a146a3a811f018aea12cd923c76
jwoo820 <pjwoo820@gmail.com>
2021-08-04 19:25:25 +0900

    임시페이지

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import img from "./Image/임시이미지.png";
+import styled from "styled-components";
+
+const MainImg = styled.div`
+  img {
+    text-align: center;
+  }
+`;
 
 function App() {
-  return <div>j</div>;
+  return (
+    <div>
+      <MainImg>
+        <img src={img} />
+      </MainImg>
+    </div>
+  );
 }
 
 export default App;
